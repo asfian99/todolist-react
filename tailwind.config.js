1c70f6060bb75868156b77ef93789270c4ea64b1
asfian99 <mk.asfian@gmail.com>
2021-01-14 06:17:41 +0700

    setting & reconfig tailwindcss v2 ft. craco

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
